50946c8d8d0413e6666e9bdba383e01e5daad203
donutswdad <62872229+svirant@users.noreply.github.com>
2024-05-25 11:50:03 -0700

    formatting cleanup

--- FisherPriceController.ino
+++ FisherPriceController.ino
@@ -27,11 +27,8 @@ const char *gp_serial = "Daemonbite Arcade";
 
 Gamepad_ Gamepad;           // Set up USB HID gamepad
 bool usbUpdate = false; 
-
-
 uint8_t  axis = 0x0f;
 uint8_t  axisPrev = 0x0f;
-
 uint16_t buttons = 0;
 uint16_t buttonsPrev = 0;
 
@@ -44,7 +41,6 @@ void setup()
   DDRD  &= ~B00011111; // Set PD0-PD4 as inputs
 
   Gamepad.reset();
-
 }
 
 void loop() 
@@ -53,23 +49,21 @@ void loop()
     // Read axis and button inputs (bitwise results in a 1 when button/axis pressed)
     axis = (PINF & B11110000);
     buttons = (PIND & B00011111);
-
-         
+ 
     // Has axis inputs changed?
     if(axis != axisPrev)
     {
-        // UP + DOWN = UP, SOCD (Simultaneous Opposite Cardinal Directions) Cleaner
-        if(axis & B10000000)
-          Gamepad._GamepadReport.Y = -1;
-        else if(axis & B01000000)
-          Gamepad._GamepadReport.Y = 1;
-        else
-          Gamepad._GamepadReport.Y = 0;
-        // UP + DOWN = NEUTRAL
-        //Gamepad._GamepadReport.Y = ((axis & B01000000)>>6) - ((axis & B10000000)>>7);
+      // UP + DOWN = UP, SOCD (Simultaneous Opposite Cardinal Directions) Cleaner
+      if(axis & B10000000)
+        Gamepad._GamepadReport.Y = -1;
+      else if(axis & B01000000)
+        Gamepad._GamepadReport.Y = 1;
+      else
+        Gamepad._GamepadReport.Y = 0;
       
       // LEFT + RIGHT = NEUTRAL  
-      Gamepad._GamepadReport.X = ((axis & B00010000)>>4) - ((axis & B00100000)>>5);       
+      Gamepad._GamepadReport.X = ((axis & B00010000)>>4) - ((axis & B00100000)>>5);  
+      
       axisPrev = axis;
       usbUpdate = true;
     }
@@ -88,5 +82,4 @@ void loop()
       Gamepad.send();
       usbUpdate = false;
     }
- 
 }
